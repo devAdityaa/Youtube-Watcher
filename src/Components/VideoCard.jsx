15bd745ecaf4cf0c3e4f48fcf3dece28460f12f2
devAdityaa <acolydebaian8@gmail.com>
2023-12-22 22:57:35 +0530

    Added VideoCard and Video Logic

---
+++ src/Components/VideoCard.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { Card,Typography,CardContent,CardMedia } from '@mui/material';
+import { CheckCircle } from '@mui/icons-material';
+
+const VideoCard = ({video:{id:{videoId},snippet}}) => {
+  return (
+    <Card sx={{width:{md:320, xs:'100%'},boxShadow:'none',borderRadius:1} }>
+        <Link to={videoId? `/video/${videoId}`:`/`}>
+            <CardMedia 
+            image={snippet?.thumbnails?.high?.url}
+            alt={snippet?.title}
+            sx={{width:358,height:180}}
+            />
+            <CardContent 
+            sx={{backgroundColor:'#1e1e1e',height:'106px'}}
+            >
+                <Link 
+                to={videoId? `/video/${videoId}`:`/`}>
+                    <Typography 
+                    variant='subtitle1' 
+                    fontWeight='bold' 
+                    color='#fff'>
+                        {snippet?.title.slice(0,60)+"..."}
+                    </Typography>
+                </Link>
+                <Link 
+                to={snippet.channelId? `/channel/${snippet?.channelId}`:`/`}>
+                    <Typography 
+                    variant='subtitle2' 
+                    fontWeight='bold' 
+                    color='gray'>
+                        {snippet?.channelTitle}
+                        <CheckCircle sx={{fontSize:12, color:'gray', ml:'5px'}}/>
+                    </Typography>
+                </Link>
+            </CardContent>
+        </Link>
+    </Card>
+  )
+}
+
+export default VideoCard
